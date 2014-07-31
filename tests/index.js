e44ce521bef7331b84960caa14826c8080cd9c8d
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-30 20:15:44 -0700

    (test) Added tests for all features.

--- tests/index.js
+++ tests/index.js
@@ -31,13 +31,17 @@ describe('double-list', function () {
   describe('deletion', function () {
     describe('unshift', function () {
       it('should remove from the head of a list', function () {
-
+        var list = List.fromArray([1, 2, 3, 4, 5]);
+        list.unshift();
+        demand(list.toArray()).to.eql([2, 3, 4, 5]);
       });
     });
 
     describe('pop', function () {
       it('should remove from the end of a list', function () {
-
+        var list = List.fromArray([1, 2, 3, 4, 5]);
+        list.pop();
+        demand(list.toArray()).to.eql([1, 2, 3, 4]);
       });
     });
   });
@@ -45,13 +49,16 @@ describe('double-list', function () {
   describe('interaction with arrays', function () {
     describe('toArray', function () {
       it('should convert to an array', function () {
-
+        var list = new List();
+        list.push(1); list.push(2); list.push(3);
+        demand(list.toArray()).eql([1, 2, 3]);
       });
     });
 
     describe('fromArray', function () {
       it('should convert from an array', function () {
-
+        var list = List.fromArray([1, 2, 3]);
+        demand(list.toArray()).eql([1, 2, 3]);
       });
     });
   });
@@ -59,13 +66,17 @@ describe('double-list', function () {
   describe('movement', function () {
     describe('moveToFront', function () {
       it('should move a node to the front', function () {
-
+        var list = List.fromArray([1, 2, 3]);
+        list.moveToFront(list.head.next);
+        demand(list.toArray()).eql([2, 1, 3]);
       });
     });
 
     describe('moveToEnd', function () {
       it('should move a node to the end', function () {
-
+        var list = List.fromArray([1, 2, 3]);
+        list.moveToEnd(list.head.next);
+        demand(list.toArray()).eql([1, 3, 2]);
       });
     });
   });
@@ -74,13 +85,27 @@ describe('double-list', function () {
     describe('insertion around', function () {
       describe('insertAfter', function () {
         it('should insert a node after this one', function () {
-
+          var list = List.fromArray([1, 2, 3]);
+          list.head.next.insertAfter(4);
+          demand(list.toArray()).eql([1, 2, 4, 3]);
         });
       });
 
       describe('insertBefore', function () {
         it('should insert a node before this one', function () {
+          var list = List.fromArray([1, 2, 3]);
+          list.head.next.insertBefore(4);
+          demand(list.toArray()).eql([1, 4, 2, 3]);
+        });
+      });
+    });
 
+    describe('deletion around', function () {
+      describe('delete', function () {
+        it('should delete itself', function () {
+          var list = List.fromArray([1, 2, 3]);
+          list.head.next.delete();
+          demand(list.toArray()).eql([1, 3]);
         });
       });
     });
